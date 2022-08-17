b94d21625781f25b16a572235c91c5fb7cae8964
nikkscode <dominik.kisiala@gmail.com>
2022-08-17 23:00:38 +0200

    Created a LimitsController and a Limits Bean

---
+++ limits-service/src/main/java/com/nikkscode/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,14 @@
+package com.nikkscode.microservices.limitsservice.controller;
+
+import com.nikkscode.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(1,1000);
+    }
+}
