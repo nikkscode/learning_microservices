a11370042e7316fcd6f791173f3e5a00ce708309
nikkscode <dominik.kisiala@gmail.com>
2022-08-18 21:06:06 +0200

    created the model for currency-conversion and a hardcoded restresponse

---
+++ currency-conversion-service/src/main/java/com/nikkscode/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,17 @@
+package com.nikkscode.microservices.currencyconversionservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+
+        return new CurrencyConversion(1111L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+}
