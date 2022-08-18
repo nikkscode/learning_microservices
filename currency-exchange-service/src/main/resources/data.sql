3b6e8cc78569a1b7af511e398def1c94c56e594e
nikkscode <dominik.kisiala@gmail.com>
2022-08-18 20:29:10 +0200

    added h2 in-memory-db, created entity, filled with data from sql-sript and set property to create table before executing sql script

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR',65,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','PLN',75,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'AUD','USD',10,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10004,'USD','AUD',65,'');
