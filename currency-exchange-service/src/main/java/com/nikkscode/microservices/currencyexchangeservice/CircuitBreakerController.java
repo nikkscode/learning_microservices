ecee584e34c845796d9e739d74c85f338acb6ece
nikkscode <dominik.kisiala@gmail.com>
2022-08-19 21:20:32 +0200

    experimented with circuit breaking and durations and created custom fallbackMethod

--- currency-exchange-service/src/main/java/com/nikkscode/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/nikkscode/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,13 +1,27 @@
 package com.nikkscode.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
-    @GetMapping("/sample")
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi(){
-        return "Sample API";
+        logger.info("Sample Api received");
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dumy-url",String.class);
+        return responseEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception ex){
+        return "fallbacl-response";
     }
 }
