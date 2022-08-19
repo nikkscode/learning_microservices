2e3a9f174172cf7b118b23d60359e8222218c043
nikkscode <dominik.kisiala@gmail.com>
2022-08-19 21:00:31 +0200

    loaded dependencies for a circuit breaker

--- currency-exchange-service/src/main/java/com/nikkscode/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/nikkscode/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,2 +1,13 @@
-package com.nikkscode.microservices.currencyexchangeservice;public class CircuitBreakerController {
+package com.nikkscode.microservices.currencyexchangeservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+
+    @GetMapping("/sample")
+    public String sampleApi(){
+        return "Sample API";
+    }
 }
