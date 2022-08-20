0457e8da697473a997b15b2d2da7fa6a437c024b
nikkscode <dominik.kisiala@gmail.com>
2022-08-20 14:35:04 +0200

    connected microservices and apiGateway with zipkin

--- currency-conversion-service/src/main/java/com/nikkscode/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/nikkscode/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package com.nikkscode.microservices.currencyconversionservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +14,14 @@ public class CurrencyConversionController {
     @Autowired
     CurrencyExchangeProxy proxy;
 
+    Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
                                                           @PathVariable String to,
                                                           @PathVariable BigDecimal quantity){
 
+        logger.info("retrieveExChangeValue called with {} to {}, quantity {}", from, to, quantity);
         CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 
         return new CurrencyConversion(currencyConversion.getId(),
